6348c52c31ca10fe2b88c73d4fb4eeffe1a09700
jcLucio <c.lucio3@gmail.com>
2020-02-27 16:24:46 -0600

    Add initial /campggrounds functionality

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('landing');
 });
+app.get('/campgrounds', (req, res) => {
+    const campgrounds = [
+        {name: 'Honey Badger Creek', image: 'https://source.unsplash.com/iZ4yhyDB-dQ'},
+        {name: 'Sleepy Bear Hill', image: 'https://source.unsplash.com/Hxs6EAdI2Q8'},
+        {name: 'Winter Fox Lake', image: 'https://source.unsplash.com/Czw5tWFGNOI'},
+    ];
+    res.render('campgrounds', {campgrounds: campgrounds});
+});
 
 app.listen(3000, () => {
     console.log('Started yelp-camp server');
