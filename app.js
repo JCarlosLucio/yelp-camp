141f7b1c854ad755c56964f4e22c703da5082c3b
jcLucio <c.lucio3@gmail.com>
2020-02-28 16:20:11 -0600

    Add post logic to add new campground

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const campgrounds = [ //Temporary
+    { name: 'Honey Badger Creek', image: 'https://source.unsplash.com/iZ4yhyDB-dQ' },
+    { name: 'Sleepy Bear Hill', image: 'https://source.unsplash.com/Hxs6EAdI2Q8' },
+    { name: 'Winter Fox Lake', image: 'https://source.unsplash.com/Czw5tWFGNOI' },
+];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
@@ -9,19 +15,16 @@ app.get('/', (req, res) => {
     res.render('landing');
 });
 app.get('/campgrounds', (req, res) => {
-    const campgrounds = [
-        { name: 'Honey Badger Creek', image: 'https://source.unsplash.com/iZ4yhyDB-dQ' },
-        { name: 'Sleepy Bear Hill', image: 'https://source.unsplash.com/Hxs6EAdI2Q8' },
-        { name: 'Winter Fox Lake', image: 'https://source.unsplash.com/Czw5tWFGNOI' },
-    ];
     res.render('campgrounds', { campgrounds: campgrounds });
-});
-
-app.post('/campgrounds', (req, res) => {
-    res.send('HIT POST ROUTE');
 });
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
+});
+app.post('/campgrounds', (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = { name: name, image: image };
+    campgrounds.push(newCampground);
 });
 
 app.listen(3000, () => {
