e7648833ee7802132f409f60b2a560d96b9ee0f7
jcLucio <c.lucio3@gmail.com>
2020-03-01 17:00:58 -0600

    Add findById logic to Campground

--- app.js
+++ app.js
@@ -85,6 +85,13 @@ app.get('/campgrounds/new', (req, res) => {
 // SHOW - Shows more info about one campground
 app.get('/campgrounds/:id', (req, res) => {
     // find the campground with provided ID
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('show', { campground: foundCampground });
+        };
+    });
     // render show template with that campground
     res.render('show');
 });
