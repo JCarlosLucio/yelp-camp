d684ea8047699710242a1db5f9ed44529d68dc7e
jcLucio <c.lucio3@gmail.com>
2020-03-06 15:45:06 -0600

    Add longer description to campgrounds

--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ const seeds = [
     {
         name: 'Honey Badger Creek',
         image: 'https://source.unsplash.com/iZ4yhyDB-dQ',
-        description: 'blah blah blah'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo. Ultricies mi eget mauris pharetra et ultrices neque ornare. Felis imperdiet proin fermentum leo vel orci porta non. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Aenean pharetra magna ac placerat vestibulum. Gravida rutrum quisque non tellus. Turpis egestas integer eget aliquet. Sit amet aliquam id diam maecenas. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.'
     },
     {
         name: 'Sleepy Bear Hill',
         image: 'https://source.unsplash.com/Hxs6EAdI2Q8',
-        description: 'bleh bleh bleh'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo. Ultricies mi eget mauris pharetra et ultrices neque ornare. Felis imperdiet proin fermentum leo vel orci porta non. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Aenean pharetra magna ac placerat vestibulum. Gravida rutrum quisque non tellus. Turpis egestas integer eget aliquet. Sit amet aliquam id diam maecenas. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.'
     },
     {
         name: 'Winter Fox Lake',
         image: 'https://source.unsplash.com/Czw5tWFGNOI',
-        description: 'bluh bluh bluh'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo. Ultricies mi eget mauris pharetra et ultrices neque ornare. Felis imperdiet proin fermentum leo vel orci porta non. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Aenean pharetra magna ac placerat vestibulum. Gravida rutrum quisque non tellus. Turpis egestas integer eget aliquet. Sit amet aliquam id diam maecenas. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.'
     }
 ];
 
