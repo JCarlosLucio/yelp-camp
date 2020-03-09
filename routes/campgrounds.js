8fc10bc76c3337289c8ef87fe60a0753be3ffc75
jcLucio <c.lucio3@gmail.com>
2020-03-08 23:19:35 -0600

    Add isLoggedIn middleware to create and new routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
 });
 
 // CREATE - Add new campground to DB
-router.post('/', (req, res) => {
+router.post('/', isLoggedIn, (req, res) => {
     //get data from form and add to campgrounds array
     let name = req.body.name;
     let image = req.body.image;
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
 });
 
 // NEW - Show form to create new campground
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
@@ -61,5 +61,11 @@ router.get('/:id', (req, res) => {
         };
     });
 });
-
+// isLoggedIn - middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 module.exports = router;
