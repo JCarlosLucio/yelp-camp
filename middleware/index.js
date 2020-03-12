ef4bfb8b2572fa67c23d7b0fd143a5393ed29c4e
jcLucio <c.lucio3@gmail.com>
2020-03-11 23:08:15 -0600

    Add middleware/index.js to refactor middleware from routes

---
+++ middleware/index.js
@@ -0,0 +1,58 @@
+const Campground = require('../models/campground');
+const Comment = require('../models/comment');
+
+// all the middleware goes here
+const middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function (req, res, next) {
+    // is user logged in
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            if (err) {
+                res.redirect('back');
+            } else {
+                //does user own the campground?
+                // foundCampground is not equal to req.user._id in Type so...
+                // don't use ===, use .equals() as below
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            };
+        });
+        // if not, redirect
+    } else {
+        res.redirect('back');
+    }
+}
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+    // is user logged in
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                res.redirect('back');
+            } else {
+                //does user own the comment?
+                // foundComment is not equal to req.user._id in Type so...
+                // don't use ===, use .equals() as below
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            };
+        });
+        // if not, redirect
+    } else {
+        res.redirect('back');
+    }
+}
+middlewareObj.isLoggedIn = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+module.exports = middlewareObj;
